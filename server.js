58ff695443fca5fd98320967c21eb95ab3f1a186
amrhym <amrmostafa@amrs-MacBook-Pro.local>
2016-04-01 03:30:22 +0300

    1

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get('/', function (req, res) {
 });
 
 // start server
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
